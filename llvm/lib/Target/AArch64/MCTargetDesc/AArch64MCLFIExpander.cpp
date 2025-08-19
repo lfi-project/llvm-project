6f62734b8689b82f79ceaf35a637d486038aab86
Zachary Yedidia <zyedidia@gmail.com>
2025-08-19 15:24:08 -0700

    [LFI] [AArch64] LFI rewrites for indirect branches

--- llvm/lib/Target/AArch64/MCTargetDesc/AArch64MCLFIExpander.cpp
+++ llvm/lib/Target/AArch64/MCTargetDesc/AArch64MCLFIExpander.cpp
@@ -28,18 +28,51 @@ using namespace llvm;
 
 #define DEBUG_TYPE "lfi"
 
+static MCRegister LFIAddrReg = AArch64::X28;
+static MCRegister LFIBaseReg = AArch64::X27;
+
 bool AArch64::AArch64MCLFIExpander::isValidScratchRegister(MCRegister Reg) const {
-  return false;
+  return Reg != AArch64::SP;
+}
+
+static void emitAddMask(MCRegister Dest, MCRegister Src, MCStreamer &Out,
+                        const MCSubtargetInfo &STI) {
+  MCInst Add;
+  Add.setOpcode(AArch64::ADDXrx);
+  Add.addOperand(MCOperand::createReg(Dest));
+  Add.addOperand(MCOperand::createReg(LFIBaseReg));
+  Add.addOperand(MCOperand::createReg(getWRegFromXReg(Src)));
+  Add.addOperand(MCOperand::createImm(AArch64_AM::getArithExtendImm(AArch64_AM::UXTW, 0)));
+  Out.emitInstruction(Add, STI);;
+}
+
+static void emitBranch(unsigned int Opcode, MCRegister Target, MCStreamer &Out,
+                       const MCSubtargetInfo &STI) {
+  MCInst Branch;
+  Branch.setOpcode(Opcode);
+  Branch.addOperand(MCOperand::createReg(Target));
+  Out.emitInstruction(Branch, STI);
 }
 
 void AArch64::AArch64MCLFIExpander::expandIndirectBranch(const MCInst &Inst,
                                                   MCStreamer &Out,
                                                   const MCSubtargetInfo &STI,
-                                                  bool isCall) {
+                                                  bool IsCall) {
+  (void) IsCall;
+
+  assert(Inst.getOperand(0).isReg());
+  MCRegister BranchReg = Inst.getOperand(0).getReg();
+
+  emitAddMask(LFIAddrReg, BranchReg, Out, STI);
+  emitBranch(Inst.getOpcode(), LFIAddrReg, Out, STI);
 }
 
 void AArch64::AArch64MCLFIExpander::expandCall(const MCInst &Inst, MCStreamer &Out,
                                         const MCSubtargetInfo &STI) {
+  if (Inst.getOperand(0).isReg())
+    expandIndirectBranch(Inst, Out, STI, true);
+  else
+    Out.emitInstruction(Inst, STI);
 }
 
 void AArch64::AArch64MCLFIExpander::expandReturn(const MCInst &Inst, MCStreamer &Out,
@@ -71,9 +104,38 @@ void AArch64::AArch64MCLFIExpander::expandLoadStore(const MCInst &Inst,
 
 void AArch64::AArch64MCLFIExpander::doExpandInst(const MCInst &Inst, MCStreamer &Out,
                                           const MCSubtargetInfo &STI) {
+  if (isReturn(Inst))
+    return expandReturn(Inst, Out, STI);
+
+  if (isIndirectBranch(Inst))
+    return expandIndirectBranch(Inst, Out, STI, false);
+
+  if (isCall(Inst))
+    return expandCall(Inst, Out, STI);
+
+  if (isBranch(Inst))
+    return Out.emitInstruction(Inst, STI);
+
+  if (mayAffectControlFlow(Inst))
+    return expandControlFlow(Inst, Out, STI);
+
+  if (mayModifyStack(Inst))
+    return expandStackManipulation(Inst, Out, STI);
+
+  if (mayLoad(Inst) || mayStore(Inst))
+    return expandLoadStore(Inst, Out, STI);
+
+  return Out.emitInstruction(Inst, STI);
 }
 
 bool AArch64::AArch64MCLFIExpander::expandInst(const MCInst &Inst, MCStreamer &Out,
                                         const MCSubtargetInfo &STI) {
-  return false;
+  if (Guard)
+    return false;
+  Guard = true;
+
+  doExpandInst(Inst, Out, STI);
+
+  Guard = false;
+  return true;
 }
