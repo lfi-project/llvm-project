3b331ae2f31138555cfa279a1115e39b3abe2b64
Zachary Yedidia <zyedidia@gmail.com>
2025-08-21 14:24:56 -0700

    [LFI] [AArch64] Delete uses of reserved regs by default

--- llvm/test/MC/AArch64/LFI/reserved.s
+++ llvm/test/MC/AArch64/LFI/reserved.s
@@ -1,12 +1,21 @@
-// RUN: not llvm-mc -filetype asm -triple aarch64_lfi %s 2> %t
+// RUN: llvm-mc -filetype asm -triple aarch64_lfi %s 2> %t
 // RUN: FileCheck --check-prefix=CHECK-ERROR %s <%t
 
 mov x28, x0
-// CHECK-ERROR: error: illegal modification of reserved LFI register
+// CHECK:       mov x26, x0
+// CHECK-ERROR: warning: deleting modification of reserved LFI register
 // CHECK-ERROR:        mov x28, x0
 // CHECK-ERROR:        ^
 
-ldr x27, [sp]
-// CHECK-ERROR: error: illegal modification of reserved LFI register
-// CHECK-ERROR:        ldr x27, [sp]
+ldr x27, [x0]
+// CHECK:       ldr xzr, [x27, w0, uxtw]
+// CHECK-ERROR: warning: deleting modification of reserved LFI register
+// CHECK-ERROR:        ldr x27, [x0]
+// CHECK-ERROR:        ^
+
+ldp x27, x28, [x0]
+// CHECK:       add x28, x27, w0, uxtw
+// CHECK-NEXT:  ldp xzr, x26, [x28]
+// CHECK-ERROR: warning: deleting modification of reserved LFI register
+// CHECK-ERROR:        ldp x27, x28, [x0]
 // CHECK-ERROR:        ^
