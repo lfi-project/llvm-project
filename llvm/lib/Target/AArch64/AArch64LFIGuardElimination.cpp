7e3029381e335efd3f143c242dbd0d6151e5d8cb
Zachary Yedidia <zyedidia@gmail.com>
2025-08-24 21:55:23 -0700

    [LFI] [AArch64] Start LFI guard elimination pass

--- llvm/lib/Target/AArch64/AArch64LFIGuardElimination.cpp
+++ llvm/lib/Target/AArch64/AArch64LFIGuardElimination.cpp
@@ -15,6 +15,8 @@ using namespace llvm;
 
 namespace {
 class AArch64LFIGuardElimination : public MachineFunctionPass {
+  const AArch64InstrInfo *TII;
+
 public:
   static char ID;
   AArch64LFIGuardElimination() : MachineFunctionPass(ID) {}
@@ -30,17 +32,50 @@ public:
     AU.addRequired<MachineLoopInfoWrapperPass>();
     MachineFunctionPass::getAnalysisUsage(AU);
   }
+
+private:
+  void emitNoExpand(MachineBasicBlock &MBB, MachineInstr &MI);
+  void emitExpand(MachineBasicBlock &MBB, MachineInstr &MI);
 };
 
 char AArch64LFIGuardElimination::ID = 0;
 
 } // end anonymous namespace
 
+void AArch64LFIGuardElimination::emitNoExpand(MachineBasicBlock &MBB, MachineInstr &MI) {
+  BuildMI(MBB, MI, MI.getDebugLoc(), TII->get(TargetOpcode::INLINEASM))
+    .addExternalSymbol(".no_expand");
+}
+
+void AArch64LFIGuardElimination::emitExpand(MachineBasicBlock &MBB, MachineInstr &MI) {
+  BuildMI(MBB, MI, MI.getDebugLoc(), TII->get(TargetOpcode::INLINEASM))
+    .addExternalSymbol(".expand");
+}
+
 bool AArch64LFIGuardElimination::runOnMachineFunction(MachineFunction &MF) {
   bool Changed = false;
   LLVM_DEBUG(
       dbgs() << "Running AArch64 LFI Guard Elimination Pass on function: "
              << MF.getName() << "\n");
+
+  TII = static_cast<const AArch64InstrInfo *>(MF.getSubtarget().getInstrInfo());
+
+  for (auto &MBB : MF) {
+    for (auto &MI : llvm::make_early_inc_range(MBB)) {
+      // TODO: Expand non RoW load/stores.
+    }
+    for (auto &MI : llvm::make_early_inc_range(MBB)) {
+      // TODO: Eliminate redundant guard instructions.
+      //
+      // Approach:
+      // 1. Find current guard instruction.
+      // 2. For each instruction:
+      // * if it modifies guarded reg: remove currently active guard instruction.
+      // * if it's a guard:
+      // ** if it's the same as the active guard: eliminate it
+      // ** if it's different, reset active guard
+    }
+  }
 
   return Changed;
 }
